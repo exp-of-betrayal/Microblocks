8f8e67a839ed090ef98d2ab5b14419755fc06543
exp-of-betrayal <119005542+exp-of-betrayal@users.noreply.github.com>
2022-12-27 21:35:17 +0400

    Add files via upload
    Работающая форма логин&пароль

---
+++ 4.0/sql_settings.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+connection = sqlite3.connect("database.sqlite")
+cursor = connection.cursor()
+
+cursor.execute("""
+create table data
+(
+    login    text not null
+        constraint data_pk
+            primary key,
+    password text not null,
+    code text
+);
+""")
+cursor.execute("""
+create unique index data_login_uindex
+    on data (login);
+""")
+connection.commit()
+connection.close()
